f37091d197f1d7f34184a3744b909f7c03590875
Lucas Dobler <e1127223@student.tuwien.ac.at>
2015-03-29 21:32:31 +0200

    logger

--- SearchSystem/src/at/tuwien/bss/SearchSystem.java
+++ SearchSystem/src/at/tuwien/bss/SearchSystem.java
@@ -7,9 +7,12 @@ import at.tuwien.bss.documents.DocumentCollection;
 import at.tuwien.bss.index.Indexer;
 import at.tuwien.bss.index.IndexerBag;
 import at.tuwien.bss.index.IndexerBi;
+import at.tuwien.bss.logging.SSLogger;
 import at.tuwien.bss.parse.Parser;
 
 public class SearchSystem {
+	
+	private static final SSLogger LOGGER = SSLogger.getLogger();
 	
 	public static void main(String[] args) {
 		
@@ -18,48 +21,42 @@ public class SearchSystem {
 	}
 	
 	public SearchSystem() {
+		
 		documentCollection = new DocumentCollection();
 		
-		log("Current Working Dir: "+System.getProperty("user.dir"));
+		LOGGER.log("Current Working Dir: "+System.getProperty("user.dir"));
 		
 		// documents are placed in the subset folder
 		documentCollection.importFolder(".\\..\\..\\subset");
 		
-		log("imported "+documentCollection.getCount()+" documents");
+		LOGGER.log("imported "+documentCollection.getCount()+" documents");
 	}
 	
 	private void test() {
 		
-		// test parser by parse document with ID 0, 1, 2
+		// test parser by parse document with ID 0 - 10
+		
+		LOGGER.logTime("START");
 		
 		Parser parser = new Parser();
-		Indexer indexerBoW = new IndexerBi();
+		Indexer indexerBoW = new IndexerBag();
 		
-		for(int i = 0; i < 10; i++) {
-			ArrayList<String> terms = null;
+		for(int i = 0; i < documentCollection.getCount(); i++) {
+			
 			try {
-				terms = parser.parse(documentCollection.getContent(i));
+				ArrayList<String> terms = parser.parse(documentCollection.getContent(i));
+				LOGGER.logTime("document "+ i +" parsed");
+				indexerBoW.add(terms, i);
+				LOGGER.logTime("document "+ i +" indexed");
+			
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
-			// write out terms
-
-			log("Terms:");
-			for (String t : terms) {
-				log(t);
-			}
-
-			indexerBoW.add(terms, i);
 		}
+		
+		LOGGER.logTime("FINISH");
 
-		log("******************************************");
-		log(indexerBoW.getIndex().print());
-	}
-	
-	private static void log(String s) {
-		System.out.println(s);
+		//LOGGER.log(indexerBoW.getIndex().print());
 	}
 
 	private DocumentCollection documentCollection = new DocumentCollection();
---
+++ SearchSystem/src/at/tuwien/bss/logging/SSLogger.java
@@ -0,0 +1,61 @@
+package at.tuwien.bss.logging;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SSLogger {
+	
+	private File logFile;
+	
+	private SSLogger() {
+
+		try {
+			logFile = new File("log/log.txt");
+			if (logFile.exists()) {
+				logFile.delete();
+			}
+			logFile.createNewFile();
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static SSLogger instance;
+	public static SSLogger getLogger() {
+		
+		if(instance == null) {
+			instance = new SSLogger();
+		}
+		
+		return instance;
+	}
+	
+
+	public void log(String message) {
+		write(message);
+	}
+	
+	private SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss.SSS");
+	public void logTime(String message) {
+		write(message +": \t"+ df.format(new Date().getTime()));
+	}
+	
+	private void write(String s) {
+		System.out.println(s);
+		
+		/*
+		try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(logFile, true)))) {
+		    out.println(s);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		*/
+	}
+}
